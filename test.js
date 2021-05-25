57527d626eff53d2291b6fff9dffdef7494dff09
CassandraVn <cassandra.vaslon2021@campus-eni.fr>
2021-05-25 11:43:25 +0200

    bam fini

---
+++ test.js
@@ -0,0 +1,20 @@
+function calculer() {
+    let poids = document.getElementById("poids").value;
+    let taille = document.getElementById("taille").value;
+
+    //document.getElementById('poids').value='';
+    //document.getElementById('taille').value='';
+
+    let imc = poids / (taille*taille);
+    document.getElementById('imc').innerHTML = imc.toFixed(1);
+
+    if (imc < 18.5) {
+        document.getElementById('tranche').innerHTML = "maigreur";
+    }
+    else if (imc > 18.5 && imc < 25) {
+        document.getElementById('tranche').innerHTML = "normal";
+    }
+    else if (imc > 25) {
+        document.getElementById('tranche').innerHTML = "surpoids";
+    }
+}
